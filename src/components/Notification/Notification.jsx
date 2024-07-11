dde15dc0e6954f459aaab35ee54b57a27f53f91c
GalinaMykhailenko <pr@ars.od.ua>
2024-07-11 22:25:26 +0200

    HW-02-react

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import css from "./Notification.module.css";
+
+import React from "react";
+
+function Notification() {
+  return <p className={css.text}>No feedback yet</p>;
+}
+
+export default Notification;
